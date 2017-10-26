e0850eef846725f92d5722e7f43bf6a8c0c1923b
johnpaul_elliott@hotmail.com <johnpaul_elliott@hotmail.com>
2017-10-26 12:10:45 +0100

    readme and index.js updated

--- index.js
+++ index.js
@@ -10,32 +10,31 @@ var s3Params = {
 function getConfig(callback){
    s3.getObject(s3Params, function(err, data) {
 	   if (err) console.log(err); 
-	   else 
-	   {	
-			config = JSON.parse(data.Body.toString('utf-8'));
-			callback();
-	   }
-	 });
+	   else {
+		   config = JSON.parse(data.Body.toString('utf-8'));
+		   callback();
+			}
+		   });
 }
  
 function doDNS(callback){
 	var dns = require('dns');
-    dns.resolve4(config.url, (err, addresses) => {
+	dns.resolve4(config.url, (err, addresses) => {
 		if (err) console.log(err);
-		else {	
+		else {
 			for(var i = 0; i < addresses.length; i++)
-				{		
-					if(addresses.indexOf(config.lastIP[i]) == -1) 
-					{
-						console.log("IP has changed from " + config.lastIP[0] + ", " + config.lastIP[1]);
-						console.log("to " + addresses[0] + ", " + addresses[1]);
-						callback(addresses);
-						break;
+			{
+				if(addresses.indexOf(config.lastIP[i]) == -1) 
+				{
+					console.log("IP has changed from " + config.lastIP[0] + ", " + config.lastIP[1]);
+					console.log("to " + addresses[0] + ", " + addresses[1]);
+					callback(addresses);
+					break;
 					}
 				}
-			}		
-    });
-}
+			}
+		});
+	}
 
 function sendMail(emailBody) {  
 	var ses = new aws.SES({
